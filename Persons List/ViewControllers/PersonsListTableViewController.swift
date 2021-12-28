ef6b213e2043798aa0863f011fffb41b7d7f88f5
AL-Lina <lina-sokolova00@mail.ru>
2021-12-29 01:09:02 +0300

    setup storyBoard Create UITabBarController, and two UITableViewControllers

---
+++ Persons List/ViewControllers/PersonsListTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PersonsListTableViewController.swift
+//  Persons List
+//
+//  Created by Alina Sokolova on 29.12.21.
+//
+
+import UIKit
+
+class PersonsListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "nameAndSurname", for: indexPath)
+
+        return cell
+    }
+   
+
+   
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
